3c9e937a3c8a3364fb5c195ada7854d3843cf59f
AarthyV04 <190252566+AarthyV04@users.noreply.github.com>
2025-09-11 14:04:59 +0530

    Time: 4 ms (99.73%), Space: 45.4 MB (91.17%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+         int rows = grid.length;
+        int cols = grid[0].length;
+        int perimeter = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                        perimeter += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] grid1 = {{0,1,0,0},
+                         {1,1,1,0},
+                         {0,1,0,0},
+                         {1,1,0,0}};
+        System.out.println(sol.islandPerimeter(grid1)); 
+        int[][] grid2 = {{1}};
+        System.out.println(sol.islandPerimeter(grid2)); 
+        int[][] grid3 = {{1,0}};
+        System.out.println(sol.islandPerimeter(grid3)); 
+    }
+}
