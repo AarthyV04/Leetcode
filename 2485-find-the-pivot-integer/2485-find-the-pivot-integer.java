c60be214ca1090421981e75222e6b2e169f1ab78
AarthyV04 <190252566+AarthyV04@users.noreply.github.com>
2026-02-04 10:03:50 +0530

    Time: 0 ms (100%), Space: 42.5 MB (33.31%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int pivotInteger(int n) {
+        int total = n * (n + 1) / 2;
+        int x = (int)Math.sqrt(total);
+        if (x * x == total) {
+            return x;
+        } else {
+            return -1;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(pivotInteger(n));
+    }
+}
